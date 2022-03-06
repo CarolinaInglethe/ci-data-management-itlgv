4a8ab478e436403531e49696386e720c8b2ca307
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-06 13:07:22 -0300

    NavBar - Css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div className="NavBar-page">
+    <div className="NavBar-component">
       <Link to="/ShowData">Tabela</Link>
-      <Link to="/CreateData">Cadastrar novos Dados</Link>
+      <Link to="/CreateData"> + Cadastrar novos Dados</Link>
     </div>
   );
 }
