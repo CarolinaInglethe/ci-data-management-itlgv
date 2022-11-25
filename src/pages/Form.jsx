321d0b0aaa19463deac95ecb6be3a6a7f06670a0
CarolinaInglethe <carolinainglethe@gmail.com>
2022-11-25 16:36:38 -0300

    fix - new format data

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -4,54 +4,87 @@ import InfoContext from '../context/infoContext';
 import { useNavigate } from 'react-router-dom';
 
 function Form() {
-  /*
   const { csv, setCsv } = useContext(InfoContext);
-  const [ nameInput, setNameInput ] = useState('');
-  const [ tipoInput, setTipoInput ] = useState('');
-  const [ escritorInput, setEscritorInput ] = useState('');
-  const [ lancamentoInput, setLancamentoInput] = useState('');
+  const [ inputsValues, setInputsValues ] = useState({
+    Nome: '',
+    Tipo: '',
+    Escritor: '',
+    Lançamento: '',
+    Resumo: '',
+    Pontuação: '',
+    Criticas: '',
+  })
   const navigate = useNavigate();
 
   const handleChangeInputs = ({ target: { name, value } }) => {
-    console.log(name,value);
+    setInputsValues({
+      ...inputsValues,
+      [name]: value,
+  })
   }
 
   const handleClickToSave = async () => {
     console.log('save and navigate');
+    console.log(inputsValues);
     // navigate('/ShowData');
   }
-  */
 
   return (
     <div className="Form-page">
     <NavBar />
       <p>Em manutençao...</p>
-      {/*
        <form>
+        <input
+          type="text"
+          name="Nome"
+          value={inputsValues.Nome}
+          placeholder="Nome do livro"
+          onChange={ handleChangeInputs }
+        />
+        <input
+          type="text"
+          name="Tipo"
+          value={inputsValues.Tipo}
+          placeholder="Tipo. Ex: Fantasia"
+          onChange={ handleChangeInputs }
+        />
+        <input
+          type="text"
+          name="Escritor"
+          value={inputsValues.Escritor}
+          placeholder="nome do Escritor"
+          onChange={ handleChangeInputs }
+        />
+        <input
+          type="text"
+          name="Lançamento"
+          value={inputsValues.Lançamento}
+          placeholder="Data de lançamento"
+          onChange={ handleChangeInputs }
+        />
+        <input
+          type="text"
+          name="Resumo"
+          value={inputsValues.Resumo}
+          placeholder="Escreva um Resumo"
+          onChange={ handleChangeInputs }
+        />
+        <input
+          type="text"
+          name="Pontuação"
+          value={inputsValues.Pontuação}
+          placeholder="Insira uma pontução de 0 a 10"
+          onChange={ handleChangeInputs }
+        />
          <input
           type="text"
-          name="id"
-          value={inputsValues.id}
-          placeholder="insira um Id"
-          onChange={ handleChangeInputs }
-        />
-        <input
-          type="text"
-          name="nome"
-          value={nameInput}
-          placeholder="Insira um nome"
-          onChange={ handleChangeInputs }
-        />
-        <input
-          type="text"
-          name="telefone"
-          value={lancamentoInput}
-          placeholder="insira um telefone"
+          name="Criticas"
+          value={inputsValues.Criticas}
+          placeholder="Escreva sua Critica"
           onChange={ handleChangeInputs }
         />
         <button type="button" onClick={ handleClickToSave }> Salvar </button>
       </form>
-        */} 
     </div>
   );
 }
